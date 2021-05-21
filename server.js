8b2bd8803860fa7481b4084940ff5309cbf3ffc1
CodyG <gallagher2021@gmail.com>
2021-05-20 19:35:27 -0600

    Added in more js files for login

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
@@ -15,17 +15,18 @@ const PORT = process.env.PORT || 3001;
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  // cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  // store: new SequelizeStore({
+  //   db: sequelize
+  // })
+};
 
-// app.use(session(sess));
+//middleware
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
